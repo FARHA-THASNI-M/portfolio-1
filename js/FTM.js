fe0dd1678c30b12d6b6609eeacd51ef3141ef7f7
farha_thasni_m <farhathasnim03@gmail.com>
2024-01-30 18:21:01 +0530

    added

--- js/FTM.js
+++ js/FTM.js
@@ -36,3 +36,37 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  } 
+
+  function readLess(){
+    var btnText = document.getElementById("myBtn1");
+    var moreText = document.getElementById("more");
+    var btnText1 = document.getElementById("myBtn");
+    var dots = document.getElementById("dots");
+
+    if(moreText.style.display === "inline"){
+        btnText.style.display = "inline";
+        moreText.style.display = "none";
+        dots.style.display = "inline";
+        btnText1.innerHTML = "Read more";
+    } else{
+        btnText1.style.display = "inline";
+        btnText.style.display = "none";
+  }
+  }
